5006f0497ad1812499b52d74a4f2b81584a9c48f
Andrew Villazon <andrew.villazon@gmail.com>
2020-06-09 20:45:57 +1200

    Update site meta data

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Andrew Villazon`,
+    description: `The personal website of Andrew Villazon.`,
+    author: `Andrew Villazon`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
