bbac0a1d3af326f5f35ffbbc593eb9cbbfdfe1cd
Andrew Villazon <andrew.villazon@gmail.com>
2020-06-10 22:01:37 +1200

    Create slugs on Markdown nodes

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,17 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const { createFilePath } = require(`gatsby-source-filesystem`)
 
-// You can delete this file if you're not using it
+/* 
+ Creates a slug (path) on a node based on where it is in the file
+ system. Once on the node we can use the slug to dynamically 
+ generate pages.
+*/
+exports.onCreateNode = ( {node, getNode, actions} ) => {
+    const { createNodeField } = actions
+    const contentPath = `content/blog`
+
+    if (node.internal.type === `MarkdownRemark`) {
+        const slug = createFilePath({ node, getNode, basePath: contentPath})
+        
+        createNodeField({node,name:`slug`, value: slug})
+    }
+}
