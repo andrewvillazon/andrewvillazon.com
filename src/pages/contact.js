010f4d2c734abadef5dddebd99ef5f086d85a772
Andrew Villazon <andrew.villazon@gmail.com>
2020-06-10 20:53:08 +1200

    Add contact page

---
+++ src/pages/contact.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const ContactPage = () => (
+    <Layout>
+        <SEO title="Contact" />
+        <section>
+            <h1>Contact</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+        </section>
+    </Layout>
+)
+
+export default ContactPage
