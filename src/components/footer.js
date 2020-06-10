d7a38d6569fc05303be9d462d99e42e072ec638c
Andrew Villazon <andrew.villazon@gmail.com>
2020-06-10 20:10:35 +1200

    Create footer component

---
+++ src/components/footer.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+const Footer = () => (
+    <footer>
+        <div>
+            <div>
+                Email icon
+                Github icon
+            </div>
+            <div>
+                Copyright
+            </div>
+        </div>
+    </footer>
+)
+
+export default Footer
