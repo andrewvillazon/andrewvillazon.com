96e05f3daee188778bb612e6132c2bf8109c591b
Andrew Villazon <andrew.villazon@gmail.com>
2020-06-11 21:07:15 +1200

    Add tag page creation and template

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,5 +1,6 @@
 const { createFilePath } = require(`gatsby-source-filesystem`)
 const path = require(`path`);
+const _ = require("lodash");
 
 /* 
  Creates a slug (path) on a node based on where it is in the file
@@ -7,20 +8,20 @@ const path = require(`path`);
  generate pages.
 */
 exports.onCreateNode = ({ node, getNode, actions }) => {
-    const { createNodeField } = actions
-    const contentPath = `content/blog`
+  const { createNodeField } = actions
+  const contentPath = `content/blog`
 
-    if (node.internal.type === `MarkdownRemark`) {
-        const slug = createFilePath({ node, getNode, basePath: contentPath })
+  if (node.internal.type === `MarkdownRemark`) {
+    const slug = createFilePath({ node, getNode, basePath: contentPath })
 
-        createNodeField({ node, name: `slug`, value: slug })
-    }
+    createNodeField({ node, name: `slug`, value: slug })
+  }
 }
 
 exports.createPages = async ({ graphql, actions }) => {
-    const { createPage } = actions
+  const { createPage } = actions
 
-    const result = await graphql(`
+  const result = await graphql(`
     query {
       allMarkdownRemark {
         edges {
@@ -39,16 +40,27 @@ exports.createPages = async ({ graphql, actions }) => {
     }
     `)
 
-    // Create blog post pages
-    result.data.allMarkdownRemark.edges.forEach(({ node }) => {
-        createPage(
-            {
-                path: node.fields.slug,
-                component: path.resolve(`./src/templates/post.js`),
-                context: {
-                    slug: node.fields.slug,
-                }
-            }
-        )
-    });
+  // Create blog post pages
+  result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+    createPage(
+      {
+        path: node.fields.slug,
+        component: path.resolve(`./src/templates/post.js`),
+        context: {
+          slug: node.fields.slug,
+        }
+      }
+    )
+  });
+
+  // Create tag pages
+  result.data.tagsGroup.group.forEach(tag => {
+    createPage({
+      path: `/tags/${_.kebabCase(tag.fieldValue)}/`,
+      component: path.resolve("src/templates/tag.js"),
+      context: {
+        tag: tag.fieldValue,
+      },
+    })
+  })
 }
---
+++ src/templates/tag.js
@@ -0,0 +1,42 @@
+import React from "react"
+import Layout from "../components/layout"
+import { graphql } from "gatsby"
+
+const Tags = ({ pageContext, data }) => {
+  const { tag } = pageContext
+  const postEdges = data.allMarkdownRemark.edges
+  
+  console.log(postEdges)
+
+  return (
+    <Layout>
+    <div>
+      <h1>{tag}</h1>
+    </div>
+    </Layout>
+  )
+}
+
+export default Tags
+
+export const pageQuery = graphql`
+  query($tag: String) {
+    allMarkdownRemark(
+      limit: 2000
+      sort: { fields: [frontmatter___date], order: DESC }
+      filter: { frontmatter: { tags: { in: [$tag] } } }
+    ) {
+      totalCount
+      edges {
+        node {
+          fields {
+            slug
+          }
+          frontmatter {
+            title
+          }
+        }
+      }
+    }
+  }
+`
