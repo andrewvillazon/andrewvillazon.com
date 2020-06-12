a1128f8888778484f4435e228c1bd72ae37a5c38
Andrew Villazon <andrew.villazon@gmail.com>
2020-06-12 15:47:47 +1200

    Fix error in gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,7 +23,7 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-transformer-remark`,
     `gatsby-plugin-sharp`,
-    ,`gatsby-plugin-sass`,
+    `gatsby-plugin-sass`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
