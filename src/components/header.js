433892ed57f577b880ce4e9e6b841721b0950acc
Andrew Villazon <andrew.villazon@gmail.com>
2020-06-12 20:18:14 +1200

    Style header component

--- src/components/header.js
+++ src/components/header.js
@@ -1,20 +1,25 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import "../styles/styles.scss"
 
 const Header = ({ siteTitle }) => (
-  <header>
-    <div>
-      <h1>
-        <Link to="/">{siteTitle}</Link>
-      </h1>
-      <nav>
-        <Link to="/blog/">Articles</Link>
-        <Link to="/about/">About</Link>
-        <Link to="/contact/">Contact</Link>
-      </nav>
+  <nav className="navbar is-white">
+    <div className="container">
+      <div className="navbar-brand">
+        <Link to="/" className="navbar-item">{siteTitle}</Link>
+      </div>
+      <div className="navbar-end">
+        <div className="navbar-item">
+          <div className="buttons">
+            <Link to="/blog/" className="button is-white">Articles</Link>
+            <Link to="/about/" className="button is-white">About</Link>
+            <Link to="/contact/" className="button is-white">Contact</Link>
+          </div>
+        </div>
+      </div>
     </div>
-  </header>
+  </nav>
 )
 
 Header.propTypes = {
