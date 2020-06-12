491785783338ebd56acc4d0c32c30b78a44b8408
Andrew Villazon <andrew.villazon@gmail.com>
2020-06-12 20:54:44 +1200

    Style introduction component

--- src/components/introduction.js
+++ src/components/introduction.js
@@ -1,9 +1,17 @@
 import React from "react"
+import "../styles/styles.scss"
 
 const Introduction = () => (
-    <section>
-        <h1>Hi, I'm Andrew.</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus odio, necessitatibus explicabo reiciendis molestias impedit. In nulla quo, dolor deleniti animi accusantium impedit nihil iste necessitatibus numquam eaque ducimus natus.</p>
+    <section class="section">
+        <div className="container">
+            <div className="columns">
+                <div className="column is-half">
+                    <h1 className="title is-1">Hi, I'm Andrew.</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus odio, necessitatibus explicabo reiciendis molestias impedit. In nulla quo, dolor deleniti animi accusantium impedit nihil iste necessitatibus numquam eaque ducimus natus.</p>
+                </div>
+                <div className="column"></div>
+            </div>
+        </div>
     </section>
 )
 
