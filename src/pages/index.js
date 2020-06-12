2123494d9eea37bc6d8d4641505033ff0b021613
Andrew Villazon <andrew.villazon@gmail.com>
2020-06-12 20:31:17 +1200

    Move index content to introduction component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,13 @@
 import React from "react"
 
+import Introduction from "../components/introduction"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <section>
-      <h1>Hi, I'm Andrew.</h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus odio, necessitatibus explicabo reiciendis molestias impedit. In nulla quo, dolor deleniti animi accusantium impedit nihil iste necessitatibus numquam eaque ducimus natus.</p>
-    </section>
+    <Introduction />
   </Layout>
 )
 
