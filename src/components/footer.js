77723b11d27ddaf02617655500414ceb7555b4dc
Andrew Villazon <andrew.villazon@gmail.com>
2020-06-13 20:22:02 +1200

    Style footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,15 +1,20 @@
 import React from "react"
+import { Link } from "gatsby"
+import "../styles/styles.scss"
 
 const Footer = () => (
-    <footer>
-        <div>
-            <div>
-                Email icon
-                Github icon
-            </div>
-            <div>
-                Copyright
-            </div>
+    <footer className="footer" id="page-footer">
+        <div className="container">
+            <span className="icon is-medium mr-5">
+                <Link to="/contact/">
+                    <i className="fa fa-envelope fa-2x has-text-grey-darker"></i>
+                </Link>
+            </span>
+            <span className="icon is-medium mr-5">
+                <a href="https://github.com/andrewvillazon">
+                    <i className="fa fa-github fa-2x has-text-grey-darker"></i>
+                </a>
+            </span>
         </div>
     </footer>
 )
