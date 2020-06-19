ef4cd3b50e3d09fb1fd0ebcd950556fd991f6ef8
Andrew Villazon <andrew.villazon@gmail.com>
2020-06-20 06:48:48 +1200

    Remove prismjs global options

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,12 +27,7 @@ module.exports = {
       options: {
         plugins: [
           `gatsby-remark-autolink-headers`,
-          {
-            resolve: `gatsby-remark-prismjs`,
-            options: {
-              showLineNumbers: true,
-            }
-          }
+          `gatsby-remark-prismjs`,
         ],
       },
     },
