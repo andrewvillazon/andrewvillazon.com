7d58d225bc00b3df9e1df5a406bc58e3edd84aa7
Andrew Villazon <andrew.villazon@gmail.com>
2020-06-25 21:38:19 +1200

    Add styling to footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,16 +1,23 @@
 import React from "react"
 import GatsbyLogo from "../components/gatsby-logo"
+import Octocat from "../components/octocat"
 import "../styles/main.scss"
 
 const Footer = () => (
-    <footer>
+    <footer className="footer">
         <div className="small-container">
             <div className="footer-container">
-                <i class="fa fa-envelope" aria-hidden="true"></i>
-                <i class="fa fa-github" aria-hidden="true"></i>
-                <a id="gatsby-href" href="https://www.gatsbyjs.org/" title="Made with Gatsby" target="_blank" rel="noreferrer">
-                    <GatsbyLogo />
-                </a>
+                <div>
+                    &copy; 2020 Andrew Villazon
+                </div>
+                <div className="footer-right">
+                    <a className="footer-logo" href="https://github.com/andrewvillazon" title="See the source code for this site and more on my GitHub!" target="_blank" rel="noreferrer">
+                        <Octocat />
+                    </a>
+                    <a className="footer-logo" href="https://www.gatsbyjs.org/" title="Made with Gatsby!" target="_blank" rel="noreferrer">
+                        <GatsbyLogo />
+                    </a>
+                </div>
             </div>
         </div>
     </footer>
