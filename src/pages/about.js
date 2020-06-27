b3e837bf32fab05b6d15d94870f328c29a590a61
Andrew Villazon <andrew.villazon@gmail.com>
2020-06-27 23:10:00 +1200

    Remove placeholder content in about

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,6 +2,7 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import {Link} from "gatsby"
 
 const AboutPage = () => (
     <Layout>
@@ -9,14 +10,20 @@ const AboutPage = () => (
         <section>
             <h1>About me</h1>
             <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, esse doloribus ipsa quas velit aperiam labore ipsam magni quaerat, accusantium in, incidunt voluptas dolorem. Rem consequatur nostrum eaque neque commodi?
+            I'm Andrew Villazon, a Data Analyst. Day to day I write SQL and Python in a Data Warehouse environment. I also use tools like Tableau and PowerBI for Data Visualisation.
             </p>
         </section>
         <section>
-            <h2>About this site</h2>
+            <h3>About this site</h3>
             <p>
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reprehenderit, ipsa facere? Molestiae ducimus consectetur architecto fugiat! Voluptatem reiciendis aspernatur molestiae vitae quas neque officiis inventore dolores omnis voluptas, culpa commodi.
+                This site was built with <a href="https://www.gatsbyjs.org/">Gatsby</a> and styled with <a href="https://taniarascia.github.io/primitive/">Primitive</a>. 
             </p>
+            <p>
+                If you're interested in reading about the experience of working with Gatsby I documented it <Link to="/building-my-personal-site-in-gatsby/">here</Link>.
+            </p>
+            <p>
+                The design of this site is heavily influenced by the design of <a href="https://www.taniarascia.com/">taniaracsia.com</a>, the author of Primitive. I hope she doesn't mind. I like the clean minimal design that really helps focus in on the content. I've never met Tania but have really enjoyed using her site and it's full of useful content.
+        </p>
         </section>
     </Layout>
 )
