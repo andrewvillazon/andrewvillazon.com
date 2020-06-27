9c99125b0c2f09ecb8e1810c54152113ed6e86c8
Andrew Villazon <andrew.villazon@gmail.com>
2020-06-27 22:29:13 +1200

    Add last 5 post listing to index

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,12 +3,43 @@ import React from "react"
 import Introduction from "../components/introduction"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import { Link, graphql } from "gatsby"
+import PostListing from "../components/post-listing"
 
-const IndexPage = () => (
+const IndexPage = ({data}) => {
+  const postEdges = data.allMarkdownRemark.edges
+
+  return(
   <Layout>
     <SEO title="Home" />
     <Introduction />
+    <section className="margin-top padding-top margin-bottom">
+    <h2>Latest <Link to="/articles/">Articles</Link></h2>
+    <PostListing postEdges={postEdges} />
+    </section>
   </Layout>
-)
+  )
+}
 
 export default IndexPage
+
+export const pageQuery = graphql`
+  query {
+    allMarkdownRemark(
+      limit: 5
+      sort: { fields: [frontmatter___date], order: DESC }
+    ) {
+      edges {
+        node {
+          fields {
+            slug
+          }
+          frontmatter {
+            title
+            date(formatString: "DD MMMM, YYYY")
+          }
+        }
+      }
+    }
+  }
+`
