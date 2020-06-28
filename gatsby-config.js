1a8cf2c87d26db4f866e634d341a5b70e2c1e5f2
Andrew Villazon <andrew.villazon@gmail.com>
2020-06-28 12:41:17 +1200

    Configure PWA support

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,17 +35,17 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Andrew Villazon`,
+        short_name: `Andrew Villazon`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
-        display: `minimal-ui`,
+        display: `standalone`,
         icon: `src/images/site-icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`,
   ],
 }
