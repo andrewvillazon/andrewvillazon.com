45f184b936ba3f684d6469d561f8705dd61c823e
Andrew Villazon <andrew.villazon@gmail.com>
2020-11-22 05:21:17 +1300

    Add new layout to posts

--- src/templates/post.js
+++ src/templates/post.js
@@ -3,47 +3,45 @@ import { graphql } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { Link } from "gatsby"
-const _ = require("lodash");
+const _ = require("lodash")
 
 export default function BlogPost({ data }) {
-    const post = data.markdownRemark
-    const tags = post.frontmatter.tags;
+  const post = data.markdownRemark
+  const tags = post.frontmatter.tags
 
-    return (
-        <Layout>
-            <SEO title={post.frontmatter.title} description={post.excerpt} />
-            <article>
-                <header className="post-header padding-bottom margin-bottom">
-                    <h1>{post.frontmatter.title}</h1>
-                    <p className="post-info">
-                        <small><time>{post.frontmatter.date}</time></small>
-                    </p>
-                    <div className="tag-container">
-                        {
-                            tags.map(tag => (
-                                <Link 
-                                to={`/tags/${_.kebabCase(tag)}/`}
-                                className="tag">{tag}</Link>
-                            ))
-                        }
-                    </div>
-                </header>
-                <div dangerouslySetInnerHTML={{ __html: post.html }} />
-            </article>
-        </Layout>
-    )
+  return (
+    <Layout>
+      <SEO title={post.frontmatter.title} description={post.excerpt} />
+      <article>
+        <div class="container">
+          <header>
+            <h1>Automatically deploying a Gatsby site with Github Actions</h1>
+            <p>
+              <time class="article-time">17 July, 2020</time>
+            </p>
+            <div class="flex article-tags">
+              {tags.map(tag => (
+                <Link to={`/tags/${_.kebabCase(tag)}/`}>{tag}</Link>
+              ))}
+            </div>
+          </header>
+        <div dangerouslySetInnerHTML={{ __html: post.html }} />
+        </div>
+      </article>
+    </Layout>
+  )
 }
 
 export const query = graphql`
-    query($slug: String!) {
-        markdownRemark(fields: { slug: { eq: $slug } }) {
-            html
-            frontmatter {
-                title
-                date(formatString: "DD MMMM, YYYY")
-                tags
-            }
-            excerpt
-        }
+  query($slug: String!) {
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+      html
+      frontmatter {
+        title
+        date(formatString: "DD MMMM, YYYY")
+        tags
+      }
+      excerpt
     }
+  }
 `
