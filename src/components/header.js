0a5040bf76c49ad84b5038ee1ea3a7fffd4f0d1d
Andrew Villazon <andrew.villazon@gmail.com>
2020-11-21 20:56:02 +1300

    Update to new navigation

--- src/components/header.js
+++ src/components/header.js
@@ -3,15 +3,12 @@ import PropTypes from "prop-types"
 import React from "react"
 
 const Header = ({ siteTitle }) => (
-  <nav className="navigation">
-    <div className="small-container">
-      <div className="nav-container">
-          <Link to="/" className="nav-home">{siteTitle}</Link>
-        <div className="nav-links">
-          <Link to="/articles/" className="button">Articles</Link>
-          <Link to="/about/" className="button">About</Link>
-          <Link to="/contact/" className="button">Contact</Link>
-        </div>
+  <nav>
+    <div class="container">
+      <div class="nav-container flex">
+        <Link to="/">Andrew Villazon</Link>
+        <Link to="/articles/">Articles</Link>
+        <Link to="/contact/">Contact</Link>
       </div>
     </div>
   </nav>
