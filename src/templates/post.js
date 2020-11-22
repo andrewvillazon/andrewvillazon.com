d0a3f616e1694053f5e553aef2236b3d0200e03a
Andrew Villazon <andrew.villazon@gmail.com>
2020-11-22 13:38:15 +1300

    Add missing section tag on article

--- src/templates/post.js
+++ src/templates/post.js
@@ -12,22 +12,24 @@ export default function BlogPost({ data }) {
   return (
     <Layout>
       <SEO title={post.frontmatter.title} description={post.excerpt} />
-      <article>
-        <div class="container">
-          <header>
-            <h1>Automatically deploying a Gatsby site with Github Actions</h1>
-            <p>
-              <time class="article-time">17 July, 2020</time>
-            </p>
-            <div class="flex article-tags">
-              {tags.map(tag => (
-                <Link to={`/tags/${_.kebabCase(tag)}/`} className="tag">{tag}</Link>
-              ))}
-            </div>
-          </header>
-        <div dangerouslySetInnerHTML={{ __html: post.html }} />
-        </div>
-      </article>
+      <section>
+        <article>
+          <div class="container">
+            <header>
+              <h1>Automatically deploying a Gatsby site with Github Actions</h1>
+              <p>
+                <time class="article-time">17 July, 2020</time>
+              </p>
+              <div class="flex article-tags">
+                {tags.map(tag => (
+                  <Link to={`/tags/${_.kebabCase(tag)}/`} className="tag">{tag}</Link>
+                ))}
+              </div>
+            </header>
+          <div dangerouslySetInnerHTML={{ __html: post.html }} />
+          </div>
+        </article>
+      </section>
     </Layout>
   )
 }
