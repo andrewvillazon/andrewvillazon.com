531852e94ef100c763798baad974b3894641cfd2
Andrew Villazon <andrew.villazon@gmail.com>
2020-11-23 20:44:04 +1300

    Add icons to tags

--- src/components/distinct-tags.js
+++ src/components/distinct-tags.js
@@ -1,5 +1,9 @@
 import React from "react"
 import { graphql, Link, useStaticQuery } from "gatsby"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faDatabase } from '@fortawesome/free-solid-svg-icons'
+import { faGithub, faPython, faReact} from "@fortawesome/free-brands-svg-icons"
+
 const _ = require("lodash");
 
 export default function DistinctTags() {
@@ -12,7 +16,13 @@ export default function DistinctTags() {
     `)
 
     const tags = query.allMarkdownRemark.distinct;
-    console.log(tags)
+
+    const icons = {
+        "Github": faGithub,
+        "Python": faPython,
+        "SQL Server": faDatabase,
+        "Gatsby": faReact,
+    }
 
     return (
         <>
@@ -21,7 +31,7 @@ export default function DistinctTags() {
                 <Link 
                 to={`/tags/${_.kebabCase(tag)}/`}
                 className="tag"
-                key={tag}>{tag}</Link>
+                key={tag}><FontAwesomeIcon icon={icons[tag]} /> {tag}</Link>
             ))
         }
         </>
