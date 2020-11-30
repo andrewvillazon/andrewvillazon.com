928b87bfed6ca002c302f32434abeaa0861c8ce2
Andrew Villazon <andrew.villazon@gmail.com>
2020-11-30 20:20:53 +1300

    Fix missing footer links

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,39 +1,46 @@
 import React from "react"
-import {Link} from "gatsby"
+import { Link } from "gatsby"
 import githublogo from "../images/GitHub-Mark-32px.png"
 import gatsbylogo from "../images/Gatsby_Monogram_Black.png"
 
 const Footer = () => (
-
-<footer>
-<div class="container">
-  <div class="flex footer-nav">
-      <nav class="flex">
-        {/* Internal Links */}
+  <footer>
+    <div class="container">
+      <div class="flex footer-nav">
+        <nav class="flex">
+          {/* Internal Links */}
           <Link to="/">Home</Link>
           <Link to="/articles/">Articles</Link>
           <Link to="/about/">About</Link>
           <Link to="/contact/">Contact</Link>
-      </nav>
-      {/* External Links */}
-      <nav class="flex">
-            <a href="#"
-              ><img
-                src={githublogo}
-                alt="See the source code for this site and more on my GitHub!"
-            /></a>
-            <a href="#"
-              ><img
-                src={gatsbylogo}
-                width="32px"
-                height="32px"
-                alt="Made with Gatsby!"
-            /></a>
-      </nav>
-  </div>
-  <p>&copy; Andrew Villazon 2020</p>
-</div>
-</footer>
+        </nav>
+        {/* External Links */}
+        <nav class="flex">
+          <a href="https://github.com/andrewvillazon">
+            <img
+              src={githublogo}
+              alt="See the source code for this site and more on my GitHub!"
+              title="See the source code for this site and more on my GitHub!"
+              target="_blank"
+              rel="noreferrer"
+            />
+          </a>
+          <a href="https://www.gatsbyjs.com/">
+            <img
+              src={gatsbylogo}
+              width="32px"
+              height="32px"
+              alt="Made with Gatsby!"
+              title="Made with Gatsby!"
+              target="_blank"
+              rel="noreferrer"
+            />
+          </a>
+        </nav>
+      </div>
+      <p>&copy; Andrew Villazon 2020</p>
+    </div>
+  </footer>
 )
 
 export default Footer
