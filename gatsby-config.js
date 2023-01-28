5b36d7578d008e4fd74fe788084db0cbea2b7f10
Andrew Villazon <andrew.villazon@gmail.com>
2023-01-29 05:16:29 +1300

    Add siteMetadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,8 +3,10 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `andrewvillazon`,
-    siteUrl: `https://www.yourdomain.tld`,
+    title: `Andrew Villazon`,
+    description: `Data Professional. My website and blog.`,
+    siteUrl: `https://www.andrewvillazon.com`,
+    author: "Andrew Villazon",
   },
   plugins: [
     "gatsby-plugin-image",
