56d71759b81256ac2f3472444c802d5147ba17ed
Andrew Villazon <andrew.villazon@gmail.com>
2023-02-04 15:25:24 +1300

    Style 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,46 +1,21 @@
 import * as React from "react"
 import { Link } from "gatsby"
-
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
+import Layout from "../components/Layout"
 
 const NotFoundPage = () => {
   return (
-    <main style={pageStyles}>
-      <h1 style={headingStyles}>Page not found</h1>
-      <p style={paragraphStyles}>
-        Sorry 😔, we couldn’t find what you were looking for.
-        <br />
-        {process.env.NODE_ENV === "development" ? (
-          <>
-            <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
-        <br />
-        <Link to="/">Go home</Link>.
-      </p>
-    </main>
+    <Layout>
+      <div className="container mx-auto">
+        <h1 className="mb-6 text-7xl font-extrabold tracking-tight">404</h1>
+        <p class="text-2xl font-semibold mb-12">Page not found.</p>
+        <p className="mb-8">Whoops. It looks like there's nothing here.</p>
+        <p>
+          <Link to="/" className="font-medium text-blue-600 hover:underline">
+            Head home →
+          </Link>
+        </p>
+      </div>
+    </Layout>
   )
 }
 
