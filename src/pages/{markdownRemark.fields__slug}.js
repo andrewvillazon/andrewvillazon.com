722bcbad705c205d65f9d28552a7d04a9f6f882e
Andrew Villazon <andrew.villazon@gmail.com>
2023-02-15 20:23:17 +1300

    Add prose tailwind plugin for Markdown Styling

--- src/pages/{markdownRemark.fields__slug}.js
+++ src/pages/{markdownRemark.fields__slug}.js
@@ -23,7 +23,7 @@ const MarkdownPage = ({ data }) => {
             </div>
           ) : null}
           <div
-            className="markdown"
+            className="prose prose-lg max-w-none prose-h2:border-b prose-h2:border-gray-200 prose-h2:py-2 prose-a:text-blue-600"
             dangerouslySetInnerHTML={{ __html: html }}
           />
         </div>
