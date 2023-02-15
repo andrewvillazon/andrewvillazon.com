7079a5345c015b3f5de2814e32c5ab7541604ee1
Andrew Villazon <andrew.villazon@gmail.com>
2023-02-15 19:33:18 +1300

    Add sticky footer

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -4,11 +4,11 @@ import Navigation from "./Navigation";
 
 const Layout = ({ children }) => {
   return (
-    <>
+    <div className="flex flex-col h-screen justify-between">
       <Navigation />
-      <main className="mt-24">{children}</main>
+      <main className="mt-24 mb-auto">{children}</main>
       <Footer/>
-    </>
+    </div>
   );
 };
 
