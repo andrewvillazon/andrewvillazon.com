9f78eb934e56e918016ce778bc2038b9c56e770f
Andrew Villazon <andrew.villazon@gmail.com>
2023-02-25 06:25:41 +1300

    Add gif in markdown

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,6 +21,7 @@ module.exports = {
       resolve: `gatsby-transformer-remark`,
       options: {
         plugins: [
+          `gatsby-remark-copy-linked-files`,
           "gatsby-remark-autolink-headers",
           {
             resolve: `gatsby-remark-images`,
