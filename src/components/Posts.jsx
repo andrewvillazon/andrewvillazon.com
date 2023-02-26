0d59b9c3cd45eb483d5481ad65d50f3917d02a84
Andrew Villazon <andrew.villazon@gmail.com>
2023-02-27 05:37:30 +1300

    Tweak Post Listing styles

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,58 +1,78 @@
-import { Link } from "gatsby";
-import * as React from "react";
+import { Link } from "gatsby"
+import * as React from "react"
 
 const Posts = ({ posts, groupByYears = false }) => {
   function formatDate(date) {
     const properDate = new Date(date)
-    return `${properDate.getUTCDate()} ${properDate.toLocaleString('default', { month: 'short' })}`
+    return `${properDate.getUTCDate()} ${properDate.toLocaleString("default", {
+      month: "short",
+    })}`
   }
 
   //   Re-organise if grouping by year
   if (groupByYears) {
-    const postsByYear = {};
+    const postsByYear = {}
 
     posts.forEach((post) => {
-      const year = new Date(post.frontmatter.date).getFullYear().toString();
-      postsByYear[year] = [...(postsByYear[year] || []), post];
-    });
+      const year = new Date(post.frontmatter.date).getFullYear().toString()
+      postsByYear[year] = [...(postsByYear[year] || []), post]
+    })
 
-    const years = Object.keys(postsByYear).reverse();
+    const years = Object.keys(postsByYear).reverse()
 
     return years.map((year) => (
       <>
-        <h2 className="text-3xl font-semibold mb-8 text-slate-900" key={year}>{year}</h2>
+        <h2 className="text-3xl font-semibold mb-8 text-slate-900" key={year}>
+          {year}
+        </h2>
         <div className="mb-12">
-          {postsByYear[year].map((node) => (
-            <article key={node.id}>
-              <Link
-                to={`${node.fields.slug}`}
-                className="flex justify-between items-center py-3 border-b border-gray-200"
-              >
-                <h2 className="text-lg text-slate-800 hover:text-blue-600 font-medium">{node.frontmatter.title}</h2>
-                <time>{formatDate(node.frontmatter.date)}</time>
-              </Link>
-            </article>
-          ))}
+          <ul>
+            {postsByYear[year].map((node) => (
+              <li className="py-4 border-b border-gray-200 last:border-hidden">
+                <article key={node.id}>
+                  <Link
+                    to={`${node.fields.slug}`}
+                    className="flex justify-between items-center"
+                  >
+                    <h2 className="text-lg text-slate-800 hover:text-blue-600 font-medium">
+                      {node.frontmatter.title}
+                    </h2>
+                    <time className="text-sm text-slate-400 font-semibold">
+                      {formatDate(node.frontmatter.date)}
+                    </time>
+                  </Link>
+                </article>
+              </li>
+            ))}
+          </ul>
         </div>
       </>
-    ));
+    ))
   } else {
     return (
       <div>
-        {posts.map((node) => (
-          <article key={node.id}>
-            <Link
-              to={`${node.fields.slug}`}
-              className="flex justify-between items-center py-3 border-b border-gray-200"
-            >
-              <h2 className="text-lg text-slate-800 hover:text-blue-600 font-medium">{node.frontmatter.title}</h2>
-              <time>{formatDate(node.frontmatter.date)}</time>
-            </Link>
-          </article>
-        ))}
+        <ul>
+          {posts.map((node) => (
+            <li className="py-4 border-b border-gray-200 last:border-hidden">
+              <article key={node.id}>
+                <Link
+                  to={`${node.fields.slug}`}
+                  className="flex justify-between items-center"
+                >
+                  <h2 className="text-lg text-slate-800 hover:text-blue-600 font-medium">
+                    {node.frontmatter.title}
+                  </h2>
+                  <time className="text-sm text-slate-400 font-semibold">
+                    {formatDate(node.frontmatter.date)}
+                  </time>
+                </Link>
+              </article>
+            </li>
+          ))}
+        </ul>
       </div>
-    );
+    )
   }
-};
+}
 
-export default Posts;
+export default Posts
