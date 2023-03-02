90382a96489c258956e0f0bb594e063710b3ef78
Andrew Villazon <andrew.villazon@gmail.com>
2023-03-02 20:48:25 +1300

    Style 404 for Dark Mode

--- src/pages/404.js
+++ src/pages/404.js
@@ -6,11 +6,11 @@ const NotFoundPage = () => {
   return (
     <Layout>
       <div className="container mx-auto">
-        <h1 className="mb-6 text-7xl font-extrabold tracking-tight">404</h1>
-        <p class="text-2xl font-semibold mb-12">Page not found.</p>
-        <p className="mb-8">Whoops. It looks like there's nothing here.</p>
+        <h1 className="mb-6 text-7xl font-extrabold tracking-tight dark:text-slate-200">404</h1>
+        <p class="text-2xl font-semibold mb-12 dark:text-slate-200">Page not found.</p>
+        <p className="mb-10 text-xl dark:text-slate-300">Whoops. It looks like there's nothing here.</p>
         <p>
-          <Link to="/" className="font-medium text-blue-600 hover:underline">
+          <Link to="/" className="font-medium text-blue-600 hover:underline dark:text-blue-500">
             Head home →
           </Link>
         </p>
