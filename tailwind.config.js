74c5c04dc1c9f0603981bdb9506d98f430185118
Andrew Villazon <andrew.villazon@gmail.com>
2024-10-16 12:49:30 +1300

    Install fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,7 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require("tailwindcss/defaultTheme")
+
 module.exports = {
   content: [
     "./src/pages/**/*.{js,jsx,ts,tsx}",
@@ -8,7 +11,12 @@ module.exports = {
     screens: {
       xl: "800px",
     },
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Inter", ...defaultTheme.fontFamily.sans],
+        mono: ["Fira Code", ...defaultTheme.fontFamily.mono],
+      },
+    },
   },
   plugins: [require("@tailwindcss/typography")],
   darkMode: "class",
