d3f586972aa5a3d619b7c7da50fe50e7be3201a4
Andrew Villazon <andrew.villazon@gmail.com>
2024-11-02 14:33:02 +1300

    Home page Dark Mode changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,36 +14,53 @@ const IndexPage = ({ data }) => {
   return (
     <Layout>
       <section>
-        <h1 className="text-4xl mb-8 font-semibold">
-          Hi, I'm <span className="text-red-600">Andrew</span>
-        </h1>
-        <p className="mb-4 text-lg">
-          I'm a Data Engineer writing about some of the things I've learned
-          working in Data & Analytics.
-        </p>
-        <p className="mb-4 text-lg">
-          On this site, you can read some of my&nbsp;
-          <Link to="/articles" className="text-red-600">
-            articles
-          </Link>
-          , learn more&nbsp;
-          <Link to="/about" className="text-red-600">
-            about me
-          </Link>
-          , or&nbsp;
-          <Link to="/about/#contact" className="text-red-600">
-            get in touch
-          </Link>
-          !
-        </p>
+        <div className="text-gray-700 dark:text-gray-300">
+          <h1 className="text-4xl mb-8 font-semibold">
+            Hi, I'm{" "}
+            <span className="text-cyan-700 dark:text-yellow-500">Andrew</span>
+          </h1>
+          <p className="mb-4 text-lg">
+            I'm a Data Engineer writing about some of the things I've learned
+            working in Data & Analytics.
+          </p>
+          <p className="mb-4 text-lg">
+            On this site, you can read some of my&nbsp;
+            <Link
+              to="/articles"
+              className="text-cyan-700 dark:text-yellow-500 font-semibold"
+            >
+              articles
+            </Link>
+            , learn more&nbsp;
+            <Link
+              to="/about"
+              className="text-cyan-700 dark:text-yellow-500 font-semibold"
+            >
+              about me
+            </Link>
+            , or&nbsp;
+            <Link
+              to="/about/#contact"
+              className="text-cyan-700 dark:text-yellow-500 font-semibold"
+            >
+              get in touch
+            </Link>
+            !
+          </p>
+        </div>
       </section>
 
       <section>
         <div className="mt-12">
-          <h2 className="text-3xl font-semibold mb-8">Latest</h2>
+          <h2 className="text-3xl font-semibold mb-8 text-gray-700 dark:text-gray-300">
+            Latest
+          </h2>
           <Posts posts={latestPosts} />
           <div className="mt-8">
-            <Link to={"/articles"} className="text-sm text-gray-700">
+            <Link
+              to={"/articles"}
+              className="text-sm text-cyan-700 dark:text-yellow-500"
+            >
               All articles →
             </Link>
           </div>
@@ -52,7 +69,9 @@ const IndexPage = ({ data }) => {
 
       <section>
         <div className="mt-12">
-          <h2 className="text-3xl font-semibold mb-8">Explore</h2>
+          <h2 className="text-3xl font-semibold mb-8 text-gray-700 dark:text-gray-300">
+            Explore
+          </h2>
           <Tags tags={allTags} />
         </div>
       </section>
@@ -60,19 +79,18 @@ const IndexPage = ({ data }) => {
       <section>
         <div className="mt-12">
           <div>
-            <h2 className="text-3xl font-semibold mb-8">Open Source</h2>
+            <h2 className="text-3xl font-semibold mb-8 text-gray-700 dark:text-gray-300">
+              Open Source
+            </h2>
           </div>
           <div>
             {projects.map((project) => {
               return (
-                <div
-                  key={project.name}
-                  className="border border-gray-300 bg-gray-100 rounded-lg px-3 pt-2 pb-3 mb-6"
-                >
-                  <h3 className="text-lg font-semibold text-gray-900">
-                    {project.name}
+                <div key={project.name}>
+                  <h3 className="text-lg font-semibold text-gray-700 dark:text-gray-300">
+                    <a href={project.github} target="_blank" className="text-cyan-700 dark:text-yellow-500">{project.name}</a>
                   </h3>
-                  <p className="mt-4 mb-5 text-gray-600">
+                  <p className="mt-4 mb-5 text-gray-600 dark:text-gray-400 text-sm">
                     {project.description}
                   </p>
                   <div className="flex">
@@ -80,17 +98,17 @@ const IndexPage = ({ data }) => {
                       href={project.pypi}
                       target="_blank"
                       rel="noopener noreferrer"
-                      className="mr-2"
+                      className="mr-2 py-1 px-2 text-sm inline-flex bg-gray-300 border border-gray-400 rounded-md dark:border-gray-600 dark:bg-gray-700"
                     >
-                      <Boxes size={24} />
+                      <Boxes size={24} className="stroke-gray-600 dark:stroke-gray-400" />
                     </a>
                     <a
                       href={project.github}
                       target="_blank"
                       rel="noopener noreferrer"
-                      className="ml-2"
+                      className="ml-1 py-1 px-2 text-sm inline-flex bg-gray-300 border border-gray-400 rounded-md dark:border-gray-600 dark:bg-gray-600"
                     >
-                      <Github size="24" />
+                      <Github size="24" className="stroke-gray-600 dark:stroke-gray-400" />
                     </a>
                   </div>
                 </div>
