3cc425b153c0a2c909e5b088f3ad0c82ff0a821b
Andrew Villazon <andrew.villazon@gmail.com>
2024-11-02 14:28:26 +1300

    Dark Mode changes and icon refinements

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 import { Link } from "gatsby"
-import { Sunset, Shrub, FileText, UserRound, Mail } from "lucide-react"
+import { Shrub, FileText, UserRound, Mail, House } from "lucide-react"
 import DarkModeToggle from "./DarkModeToggle"
 
 const Navigation = () => {
@@ -12,16 +12,16 @@ const Navigation = () => {
 
   return (
     <section>
-      <nav className="h-16 w-full border-b border-gray-300">
+      <nav className="h-16 w-full border-b border-gray-300 dark:border-gray-700">
         <div className="container mx-auto px-5 h-full flex items-center justify-between">
           <Link to="/" className="flex-1">
-            <Shrub size="24" />
+            <House size="24" className="stroke-cyan-700 dark:stroke-yellow-500" />
             <span className="hidden">Andrew Villazon</span>
           </Link>
           <div className="flex space-x-5">
             {navItems.map((item) => (
               <Link to={item.url} key={item.url}>
-                <item.icon size={24} />
+                <item.icon size={24} className="stroke-cyan-700 dark:stroke-yellow-500" />
                 <span className="hidden">{item.text}</span>
               </Link>
             ))}
