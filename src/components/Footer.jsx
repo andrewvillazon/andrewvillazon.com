603c5599eab2a7ba6ff65814486330fc6d708d5f
Andrew Villazon <andrew.villazon@gmail.com>
2024-11-02 14:28:06 +1300

    Adjustments for Dark Mode

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,34 +5,34 @@ import { Github, FileText, UserRound, Mail, Linkedin } from "lucide-react"
 const Footer = () => {
   return (
     <section>
-      <footer className=" bg-gray-300 mt-24 border border-t border-gray-400">
+      <footer className="mt-24 border-t border-gray-300 dark:border-gray-700">
         <div className="container mx-auto px-5 py-14">
           <nav className="flex flex-wrap justify-center gap-5">
             <Link to={"/articles"}>
-              <FileText size={24} />
+              <FileText size={24} className="stroke-cyan-700 dark:stroke-yellow-500"/>
             </Link>
             <Link to={"/about"}>
-              <UserRound size={24} />
+              <UserRound size={24} className="stroke-cyan-700 dark:stroke-yellow-500" />
             </Link>
             <Link to={"/about/#contact"}>
-              <Mail size={24} />
+              <Mail size={24} className="stroke-cyan-700 dark:stroke-yellow-500" />
             </Link>
             <a
               href="https://www.linkedin.com/in/andrew-villazon/"
               target="_blank"
               rel="noopener noreferrer"
             >
-              <Linkedin size={24} />
+              <Linkedin size={24} className="stroke-cyan-700 dark:stroke-yellow-500"/>
             </a>
             <a
               href="https://github.com/andrewvillazon"
               target="_blank"
               rel="noopener noreferrer"
             >
-              <Github size={24} />
+              <Github size={24} className="stroke-cyan-700 dark:stroke-yellow-500"/>
             </a>
           </nav>
-          <div className="text-sm text-center mt-6 text-gray-500">
+          <div className="text-sm text-center mt-8 text-gray-500">
             &copy; {new Date().getFullYear()} Andrew Villazon. All rights
             reserved.
           </div>
