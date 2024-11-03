f8471865bac5166e943f9ad47df2298e6b3d5181
Andrew Villazon <andrew.villazon@gmail.com>
2024-11-03 16:58:12 +1300

    Fix missing rel

--- src/pages/index.js
+++ src/pages/index.js
@@ -88,7 +88,14 @@ const IndexPage = ({ data }) => {
               return (
                 <div key={project.name}>
                   <h3 className="text-lg font-semibold text-gray-700 dark:text-gray-300">
-                    <a href={project.github} target="_blank" className="text-cyan-700 dark:text-yellow-500">{project.name}</a>
+                    <a
+                      href={project.github}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="text-cyan-700 dark:text-yellow-500"
+                    >
+                      {project.name}
+                    </a>
                   </h3>
                   <p className="mt-4 mb-5 text-gray-600 dark:text-gray-400 text-sm">
                     {project.description}
@@ -100,7 +107,10 @@ const IndexPage = ({ data }) => {
                       rel="noopener noreferrer"
                       className="mr-2 py-1 px-2 text-sm inline-flex bg-gray-300 border border-gray-400 rounded-md dark:border-gray-600 dark:bg-gray-700"
                     >
-                      <Boxes size={24} className="stroke-gray-600 dark:stroke-gray-400" />
+                      <Boxes
+                        size={24}
+                        className="stroke-gray-600 dark:stroke-gray-400"
+                      />
                     </a>
                     <a
                       href={project.github}
@@ -108,7 +118,10 @@ const IndexPage = ({ data }) => {
                       rel="noopener noreferrer"
                       className="ml-1 py-1 px-2 text-sm inline-flex bg-gray-300 border border-gray-400 rounded-md dark:border-gray-600 dark:bg-gray-600"
                     >
-                      <Github size="24" className="stroke-gray-600 dark:stroke-gray-400" />
+                      <Github
+                        size="24"
+                        className="stroke-gray-600 dark:stroke-gray-400"
+                      />
                     </a>
                   </div>
                 </div>
