60071bfc6556d779142087d6b73b1906f9f64358
Andrew Villazon <andrew.villazon@gmail.com>
2024-11-14 18:23:54 +1300

    Tweak date

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -4,9 +4,7 @@ import * as React from "react"
 const Posts = ({ posts, groupByYears = false }) => {
   function formatDate(date) {
     const properDate = new Date(date)
-    return `${properDate.getUTCDate()} ${properDate.toLocaleString("default", {
-      month: "short",
-    })}`
+    return `${properDate.getUTCDate()} ${properDate.toLocaleString("default", {month: "short",})} ${properDate.getFullYear()}`
   }
 
   //   Re-organise if grouping by year
@@ -29,12 +27,12 @@ const Posts = ({ posts, groupByYears = false }) => {
           <div key={node.id} className="border-b border-gray-500 border-dotted last:border-hidden mt-4 md:mt-6">
             <article>
               <Link to={`${node.fields.slug}`}>
+                <time className="block text-xs text-gray-400 dark:text-gray-500 font-mono mb-2 md:text-sm">
+                  {formatDate(node.frontmatter.date)}
+                </time>
                 <h3 className="text-base font-semibold text-sky-700 dark:text-teal-500 mb-2 md:text-lg">
                   {node.frontmatter.title}
                 </h3>
-                <time className="block text-sm text-gray-400 dark:text-gray-500 font-mono mb-3 md:text-base">
-                  {formatDate(node.frontmatter.date)}
-                </time>
                 {node.frontmatter.description && (
                   <p className="text-sm text-gray-700 dark:text-gray-400 mb-3 md:text-base">
                     {node.frontmatter.description}
@@ -53,12 +51,12 @@ const Posts = ({ posts, groupByYears = false }) => {
           <div key={node.id} className="border-b border-gray-500 border-dotted last:border-hidden mt-4 md:mt-6">
             <article>
               <Link to={`${node.fields.slug}`}>
+                <time className="block text-xs text-gray-400 dark:text-gray-500 font-mono mb-2 md:text-sm">
+                  {formatDate(node.frontmatter.date)}
+                </time>
                 <h3 className="text-base font-semibold text-sky-700 mb-2 dark:text-teal-500 md:text-lg">
                   {node.frontmatter.title}
                 </h3>
-                <time className="block text-sm text-gray-400 dark:text-gray-500 font-mono mb-3 md:text-base">
-                  {formatDate(node.frontmatter.date)}
-                </time>
                 {node.frontmatter.description && (
                   <p className="text-sm text-gray-500 mb-5 dark:text-gray-400 md:text-base">
                     {node.frontmatter.description}
