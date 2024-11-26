32eada2c7670311d8a69881fb3a56299c1789846
Andrew Villazon <andrew.villazon@gmail.com>
2024-11-26 14:20:43 +1300

    Readability tweaks

--- src/pages/404.js
+++ src/pages/404.js
@@ -7,7 +7,7 @@ const NotFoundPage = () => {
   return (
     <Layout>
       <div className="container mx-auto">
-        <h1 className="mb-8 text-5xl font-bold tracking-tight text-gray-700 dark:text-gray-300 md:text-7xl md:mb-12">404</h1>
+        <h1 className="mb-5 text-5xl font-bold tracking-tight text-gray-700 dark:text-gray-300 md:text-7xl md:mb-7">404</h1>
         <p className="text-2xl font-semibold mb-8 dark:text-slate-200 md:text-3xl md:mb-12">Page not found.</p>
         <p className="mb-8 text-base dark:text-slate-300 md:text-lg md:mb-12">Whoops. It looks like there's nothing here.</p>
         <p>
