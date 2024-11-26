2b9ecb6e87f69c9ddd0f9ce14f171824a980d0d8
Andrew Villazon <andrew.villazon@gmail.com>
2024-11-26 14:25:32 +1300

    Color tweaks

--- src/pages/404.js
+++ src/pages/404.js
@@ -8,8 +8,8 @@ const NotFoundPage = () => {
     <Layout>
       <div className="container mx-auto">
         <h1 className="mb-5 text-5xl font-bold tracking-tight text-gray-700 dark:text-gray-300 md:text-7xl md:mb-7">404</h1>
-        <p className="text-2xl font-semibold mb-8 dark:text-slate-200 md:text-3xl md:mb-12">Page not found.</p>
-        <p className="mb-8 text-base dark:text-slate-300 md:text-lg md:mb-12">Whoops. It looks like there's nothing here.</p>
+        <p className="text-2xl text-gray-700 font-semibold mb-8 dark:text-gray-300 md:text-3xl md:mb-12">Page not found.</p>
+        <p className="mb-8 text-base dark:text-gray-400 md:text-lg md:mb-12">Whoops. It looks like there's nothing here.</p>
         <p>
           <Link to="/" className="text-base font-medium text-sky-700 hover:underline dark:text-teal-500">
             Head home →
